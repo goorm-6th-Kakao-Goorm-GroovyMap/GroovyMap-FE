d7f8c3b15c5d4c9dda99b31eb04ef6b4c52bd66b
gimhanyul <yuihan421@gmail.com>
2024-06-09 16:26:53 +0900

    feat: 게시글 작성 페이지 api 연결

--- app/promotion-place/write/page.tsx
+++ app/promotion-place/write/page.tsx
@@ -1,29 +1,126 @@
 'use client'
 
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, ChangeEvent, FormEvent } from 'react'
 import { IoMdSearch } from 'react-icons/io'
+import { useMutation } from '@tanstack/react-query'
+
+interface Coordinates {
+    latitude: number | null
+    longitude: number | null
+}
+
+const postFormData = async (formData: FormData): Promise<any> => {
+    const response = await fetch('/promotionboard/write', {
+        method: 'POST',
+        body: formData,
+        credentials: 'include', // 쿠키포함
+    })
+    if (!response.ok) {
+        const errorText = await response.text()
+        throw new Error(`Form submission failed: ${errorText}`)
+    }
+
+    return response.json()
+}
 
 export default function Write() {
-    const [selectedType, setSelectedType] = useState('all')
-    const [positions, setPositions] = useState(['전체'])
-
-    const positionOptions: { [key: string]: string[] } = {
-        all: ['전체'],
-        band: ['기타', '드럼', '베이스', '키보드'],
-        music: ['보컬', '작곡', '작사'],
-        dance: ['댄서', '안무가'],
-    }
+    const [selectedType, setSelectedType] = useState<string>('ALL')
+    const [address, setAddress] = useState<string>('')
+    const [fileNames, setFileNames] = useState<File[]>([])
+    const [coordinates, setCoordinates] = useState<Coordinates>({ latitude: null, longitude: null })
 
     const handleTypeChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const type = e.target.value
-        setSelectedType(type)
-        setPositions(positionOptions[type])
+        setSelectedType(e.target.value)
     }
 
+    const handleAddressChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setAddress(e.target.value)
+    }
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFileNames(Array.from(e.target.files))
+        }
+    }
+
+    const loadDaumPostcode = () => {
+        return new Promise<void>((resolve) => {
+            const script = document.createElement('script')
+            script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js'
+            script.onload = () => {
+                resolve()
+            }
+            document.head.appendChild(script)
+        })
+    }
+
+    const convertAddressToCoordinates = async (address: string) => {
+        try {
+            const response = await fetch(
+                `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURIComponent(address)}`,
+                {
+                    headers: {
+                        Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+                    },
+                },
+            )
+
+            const data = await response.json()
+
+            if (data.documents && data.documents.length > 0) {
+                const { y, x } = data.documents[0]
+                setCoordinates({ latitude: parseFloat(y), longitude: parseFloat(x) })
+            } else {
+                console.warn('주소를 찾을 수 없습니다.')
+            }
+        } catch (error) {
+            console.error('주소 변환 중 오류 발생:', error)
+        }
+    }
+
+    const handleAddressSearch = async () => {
+        await loadDaumPostcode()
+        new (window as any).daum.Postcode({
+            oncomplete: async function (data: any) {
+                const addr = data.address
+                setAddress(addr)
+                await convertAddressToCoordinates(addr)
+            },
+        }).open()
+    }
+
+    const mutation = useMutation({
+        mutationFn: (formData: FormData) => postFormData(formData),
+    })
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+
+        if (!coordinates.latitude || !coordinates.longitude) {
+            alert('주소를 입력하고 검색하세요.')
+            return
+        }
+
+        const formData = new FormData()
+        formData.append('title', (e.target as any).title.value)
+        if (fileNames.length > 0) {
+            fileNames.forEach((file) => formData.append('fileNames', file))
+        }
+        formData.append('region', (e.target as any).region.value)
+        formData.append('part', selectedType)
+        formData.append('coordinates', JSON.stringify(coordinates))
+        formData.append('content', (e.target as any).content.value)
+
+        try {
+            await mutation.mutateAsync(formData)
+            alert('Form submitted successfully!')
+        } catch (error: any) {
+            alert(`Form submission failed: ${error.message}`)
+        }
+    }
     return (
         <div className="content p-6 bg-purple-50 min-h-screen">
             <div className="content flex-1 w-full max-w-4xl mx-auto">
-                {/* 검색창 */}
                 <div className="flex justify-center items-center mb-6">
                     <div className="relative w-full">
                         <input
@@ -36,12 +133,10 @@ export default function Write() {
                         </div>
                     </div>
                 </div>
-                {/* 메뉴이름 */}
                 <header className="mb-6">
                     <h1 className="text-2xl font-bold text-purple-700">홍보게시판</h1>
                 </header>
-                {/* 글쓰기 작성 폼 */}
-                <form className="p-6 rounded border border-gray-300">
+                <form className="p-6 rounded border border-gray-300" onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label className="font-bold mb-2" htmlFor="title">
                             제목
@@ -49,40 +144,81 @@ export default function Write() {
                         <input
                             type="text"
                             id="title"
+                            name="title"
                             className="w-full border rounded p-2"
                             placeholder="제목을 입력하세요..."
                         />
                     </div>
                     <div className="flex items-center rounded-lg mb-4">
                         <label className="font-bold mr-2">지역</label>
-                        <select className="border-none rounded p-2 bg-white">
-                            <option value="all">전체</option>
-                            <option value="강남구">강남구</option>
+                        <select name="region" className="border-none rounded p-2 bg-white">
+                            <option value="ALL">전체</option>
+                            <option value="GANGNAMGU">강남구</option>
+                            <option value="GANGDONGGU">강동구</option>
+                            <option value="GANGBUKGU">강북구</option>
+                            <option value="GANGSEOGU">강서구</option>
+                            <option value="GEUMCHEONGU">금천구</option>
+                            <option value="GUROGU">구로구</option>
+                            <option value="DOBONGGU">도봉구</option>
+                            <option value="DONGDAEMUNGU">동대문구</option>
+                            <option value="DONGJAKGU">동작구</option>
+                            <option value="MAPOGU">마포구</option>
+                            <option value="SEODAEMUNGU">서대문구</option>
+                            <option value="SEOCHOGU">서초구</option>
+                            <option value="SEONGDONGGU">성동구</option>
+                            <option value="SEONGBUKGU">성북구</option>
+                            <option value="SONGPA">송파구</option>
+                            <option value="YANGCHEONGU">양천구</option>
+                            <option value="YEONGDEUNGPOGU">영등포구</option>
+                            <option value="YONGSANGU">용산구</option>
+                            <option value="EUNPYEONGGU">은평구</option>
+                            <option value="JONGNOGU">종로구</option>
+                            <option value="JUNGGU">중구</option>
+                            <option value="JUNGNANGGU">중랑구</option>
                         </select>
                     </div>
                     <div className="flex items-center rounded-lg mb-4">
                         <label className="font-bold mr-2">유형</label>
-                        <select className="border-none rounded p-2 bg-white" onChange={handleTypeChange}>
-                            <option value="all">전체</option>
-                            <option value="band">밴드</option>
-                            <option value="music">음악</option>
-                            <option value="dance">춤</option>
-                        </select>
-                        <label className="font-bold ml-2 mr-2">포지션</label>
-                        <select className="border-none rounded p-2 bg-white">
-                            {positions.map((position) => (
-                                <option key={position} value={position}>
-                                    {position}
-                                </option>
-                            ))}
+                        <select name="part" className="border-none rounded p-2 bg-white" onChange={handleTypeChange}>
+                            <option value="ALL">전체</option>
+                            <option value="BAND">밴드</option>
+                            <option value="VOCAL">음악</option>
+                            <option value="DANCE">춤</option>
                         </select>
                     </div>
+                    <div className="mb-4 flex items-center">
+                        <label className="font-bold mb-2 mr-2" htmlFor="address">
+                            주소
+                        </label>
+                        <input
+                            type="text"
+                            id="address"
+                            name="address"
+                            className="flex-grow border rounded p-2"
+                            placeholder="주소를 입력하세요..."
+                            value={address}
+                            onChange={handleAddressChange}
+                        />
+                        <button
+                            type="button"
+                            onClick={handleAddressSearch}
+                            className="ml-2 bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            주소 검색
+                        </button>
+                    </div>
                     <div className="mb-4">
-                        <label className="font-bold mb-2" htmlFor="file">
+                        <label className="font-bold mb-2" htmlFor="fileNames">
                             미디어 업로드
                         </label>
                         <p className="text-red-400">사진 또는 동영상 하나만 업로드 가능합니다.</p>
-                        <input type="file" id="file" className="w-full bg-white p-2" />
+                        <input
+                            type="file"
+                            id="fileNames"
+                            name="fileNames"
+                            className="w-full bg-white p-2"
+                            onChange={handleFileChange}
+                        />
                     </div>
                     <div className="mb-4">
                         <label className="font-bold mb-2" htmlFor="content">
@@ -90,6 +226,7 @@ export default function Write() {
                         </label>
                         <textarea
                             id="content"
+                            name="content"
                             className="w-full border rounded p-2"
                             placeholder="내용을 입력하세요..."
                         />
