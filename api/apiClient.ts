a2129d46f6b8011d03bb3c9259182972a8ab0efc
Lee Soyeon <soyeonlee2@gmail.com>
2024-06-18 15:05:37 +0900

    fix: msw 핸들러 수정

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,17 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://localhost:3000',
-
-    headers: {
-        'Content-Type': `application/json`,
-        // 'ngrok-skip-browser-warning': '69420',
-=======
     baseURL: 'http://localhost:3000', // 백엔드 서버 주소
     headers: {
         'Content-Type': 'application/json',
->>>>>>> feature/mypage
     },
 });
 
