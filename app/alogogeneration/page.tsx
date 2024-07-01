57f3d4dace66da6ba15957f4cba4998f229b9549
Lee Soyeon <soyeonlee2@gmail.com>
2024-07-01 17:58:45 +0900

    fix: ai 생성 페이지 <img> 태그를 <Image> 컴포넌트로 변경

--- app/alogogeneration/page.tsx
+++ app/alogogeneration/page.tsx
@@ -131,6 +131,8 @@ const AiLogoGeneration: React.FC = () => {
                                 <div key={index} className="flex flex-col items-center">
                                     <Image
                                         src={image}
+                                        width={96}
+                                        height={96}
                                         alt={`Generated logo ${index + 1}`}
                                         className="w-96 h-96 object-contain mb-2"
                                     />
